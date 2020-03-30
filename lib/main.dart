29db5698f0ea34c0c996767e178bf2dbbd3649a3
Iulian Oana <iulian.andreioana@gmail.com>
2020-03-30 17:02:36 +0100

    10. GridView

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(new MaterialApp(
@@ -11,70 +12,69 @@ class MyApp extends StatefulWidget {
   _State createState() => new _State();
 }
 
+class Area {
+  int index;
+  String name;
+  Color color;
+
+  Area({this.index: -1, this.name: 'Area', this.color: Colors.lightBlueAccent});
+}
+
 class _State extends State<MyApp> {
 
-  List<Step> steps;
-  int current;
+  int location;
+  List<Area> areas;
+
 
   @override
   void initState() {
-    current = 0;
-    steps = <Step>[
-      new Step(
-          title: new Text('Step 1'),
-          content: new Text('Do something for step 1'),
-          isActive: true
-      ),
-      new Step(
-          title: new Text('Step 2'),
-          content: new Text('Do something for step 2'),
-          isActive: true
-      ),
-      new Step(
-          title: new Text('Step 3'),
-          content: new Text('Do something for step 3'),
-          isActive: true
-      ),
-    ];
+    areas = new List<Area>();
+    for(int i = 1; i <= 16; i++) {
+      areas.add(new Area(index: i, name:'Area $i'));
+    }
+
+    var rng = new Random();
+    location = rng.nextInt(areas.length);
+    print('Hint: Area ${location+1}');
   }
 
-  void stepContinue() {
-    setState(() {
-      current++;
-      if(current >= steps.length) current = steps.length - 1;
-    });
+  Widget generate(int index) {
+    return new GridTile(
+      child: new Container(
+        padding: new EdgeInsets.all(5.0),
+        child: new RaisedButton(
+          onPressed: () => onPressed(index),
+          color: areas[index].color,
+          child: new Text(areas[index].name, textAlign: TextAlign.center),
+        )
+      )
+    );
   }
 
-  void stepCancel(){
+  void onPressed(int index) {
     setState(() {
-      current--;
-      if(current < 0) current = 0;
-    });
-  }
-
-  void stepTap(int index) {
-    setState(() {
-      current = index;
-    });
+      if(index == location) {
+        areas[index].color = Colors.green;
+      }else{
+        areas[index].color = Colors.red;
+      }
+    }
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text('Widgets'),
+        title: new Text('Name here'),
       ),
       body: new Container(
         padding: new EdgeInsets.all(32.0),
         child: new Center(
-          child: new Stepper(
-            steps: steps,
-            type: StepperType.vertical,
-            currentStep: current,
-            onStepCancel: stepCancel,
-            onStepContinue: stepContinue,
-            onStepTapped: stepTap,
-          ),
+          child: new GridView.count(
+            crossAxisCount: 4,
+            children: new List<Widget>.generate(areas.length, generate)
+          )
         ),
       ),
     );
