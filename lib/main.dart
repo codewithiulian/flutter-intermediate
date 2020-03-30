67e04e2ac28a45a43597cd5b9a495038fce61104
Iulian Oana <iulian.andreioana@gmail.com>
2020-03-30 10:09:33 +0100

    2. Tooltips

--- lib/main.dart
+++ lib/main.dart
@@ -13,18 +13,29 @@ class MyApp extends StatefulWidget {
 }
 
 class _State extends State<MyApp> {
+  String headingValue = '';
+
+  void iconOnPressed() => setState(() => {
+    headingValue = DateTime.now().toString()
+  });
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text('Flutter')
+        title: new Text('Widgets')
       ),
       body: new Container(
         padding: new EdgeInsets.all(32.0),
         child: new Center(
           child: new Column(
             children: <Widget>[
-              new Text('Widgets here.')
+              new Text(headingValue),
+              new IconButton(
+                icon: new Icon(Icons.timer),
+                onPressed: iconOnPressed,
+                tooltip: 'Tap me',
+              )
             ]
           )
         )
